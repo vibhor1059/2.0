53cc4ae2e6c42c4b9718c22fa5dc4a6653f07a82
vibhor1059 <143626952+vibhor1059@users.noreply.github.com>
2023-09-03 13:50:55 +0530

    Add files via upload

---
+++ INTRO.py
@@ -0,0 +1,34 @@
+
+from tkinter import * #pip install tkinter
+from PIL import Image,ImageTk,ImageSequence #pip install Pillow
+import time
+import pygame  #pip install pygame
+from pygame import mixer
+mixer.init()
+
+root = Tk()
+root.geometry("1000x500")
+
+def play_gif():
+    root.lift()
+    root.attributes("-topmost",True )
+    global img
+    img = Image.open("G:\homework\jarvis 2.0\Jarvis_Final-20230108T143612Z-001\Jarvis_Final\ironsnap2.gif")
+    lbl = Label(root)
+    lbl.place(x=0,y=0)
+    i=0
+    mixer.music.load("G:\homework\jarvis 2.0\Jarvis_Final-20230108T143612Z-001\Jarvis_Final\Startup2.mp3")
+    mixer.music.play()
+    
+    for img in ImageSequence.Iterator(img):
+        
+        img = img.resize((1000,500))
+
+        img = ImageTk.PhotoImage(img)
+        lbl.config(image=img)
+        root.update()
+        time.sleep(0.05)
+    root.destroy()
+
+play_gif()
+root.mainloop()
